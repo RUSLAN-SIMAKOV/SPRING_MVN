16abddeb3859d06b71f26352bef97a3e48295ce4
RUSLAN SIMAKOV <UA667766706@GMAIL.COM>
2020-02-13 11:53:49 +0200

    added checkstyle

--- spring_mvn/Main.java
+++ spring_mvn/Main.java
@@ -11,14 +11,12 @@ public class Main {
 	public static void main(String[] args) {
 
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-		
+
 		UserService userService = context.getBean(UserService.class);
-		
-		
+
 		userService.add(new User("First", true));
 		userService.add(new User("Second", false));
-		
-		userService.listUsers().stream().forEach(System.out::println);
 
+		userService.listUsers().stream().forEach(System.out::println);
 	}
 }
